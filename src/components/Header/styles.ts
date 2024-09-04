d4fd0783c5f19bdfe915e8cd6309d1f66db8953e
Hamid-2027 <hamidhussain2027@gmail.com>
2024-09-03 23:37:22 -0800

    bugfix(main): change contact bg color

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -169,10 +169,10 @@ export const Container = styled.header`
       top: 0;
       left: 0;
       transition: opacity 0.25s;
-      background-color: var(--green);
+      background-color: var(--blue);
 
       a.button{
-        background-color: var(--pink);
+        background-color: var(--blue);
       }
 
       &.active{
